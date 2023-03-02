4f13b30e4e12b6d8caec7cd211a1f3a0de759b82
rew asd <fork213@126.com>
2023-03-02 17:11:12 +0800

    nlog和数据库学习

---
+++ 3.Nlog_TestSql/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using Dapper;
+
+namespace _3.Nlog_TestSql
+{
+    class Program
+    {
+        public static string testConnString = "Server=localhost;User ID=postgres;password=613;Database=postgres";
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Error("1111");
+            //using (NpgsqlConnection conn = new NpgsqlConnection(testConnString))
+            //{
+            //    try
+            //    {
+            //        conn.Open();
+            //        string insertSql = string.Format(@"INSERT INTO public.logger_info(time,level,info) VALUES('{0}','{1}','{2}')", DateTime.Now.ToString("HH-mm-ss"), "Warning", "test1");
+            //        conn.Execute(insertSql);
+            //    }
+            //    catch (Exception)
+            //    {
+
+            //        throw;
+            //    }
+            //    finally
+            //    {
+            //        conn.Close();
+            //    }
+
+            //}
+            Console.ReadKey();
+        }
+    }
+}
