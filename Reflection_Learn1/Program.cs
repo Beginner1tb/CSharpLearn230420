d818b770390074da53689a4f9e608caefe792294
rew asd <fork213@126.com>
2023-04-20 15:55:33 +0800

    反射学习

---
+++ Reflection_Learn1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection_Learn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Type type = typeof(Person);
+            object person = Activator.CreateInstance(type);
+            PropertyInfo prop = type.GetProperty("Name");
+            PropertyInfo[] propertyInfos = type.GetProperties();
+            prop.SetValue(person, "John Doe");
+            Console.WriteLine(prop.GetValue(person));
+            Console.WriteLine("------------------------");
+
+
+        }
+
+  
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
