f67515b3150afd49d97997b651b106520a7c4668
rew asd <fork213@126.com>
2023-04-24 12:09:53 +0800

    C#调用C++最简单的学习

---
+++ CppInvoke1/CSharpMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CppInvoke1
+{
+   // [StructLayout(LayoutKind.Sequential)]
+
+    class CSharpMain
+    {
+        public struct MyStruct
+        {
+            public int a;
+            public int b;
+        }
+
+        public const string dllPath = @"D:\CSharp\CppBuild\x64\Debug\CppDll1.dll";
+
+        [DllImport(dllPath,CallingConvention=CallingConvention.Cdecl)]
+        private static extern int Add(int a, int b);
+
+
+        static void Main(string[] args)
+        {
+
+            int a = 1, b = 2;
+            Console.WriteLine(Add(a, b));
+        }
+    }
+}
