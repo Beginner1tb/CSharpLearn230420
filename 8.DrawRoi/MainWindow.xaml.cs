2ac1308ebc42dafb83a0fab232020e0bb11a9edc
rew asd <fork213@126.com>
2023-05-29 16:48:08 +0800

    继续画框，可以移动和拖动边框

--- 8.DrawRoi/MainWindow.xaml.cs
+++ 8.DrawRoi/MainWindow.xaml.cs
@@ -29,6 +29,13 @@ namespace _8.DrawRoi
         private Point startPoint_1;
         private FrameworkElement selectedRect;
 
+
+        //整体位移框用参数
+        private bool isDragging_rec = false;
+        private Point startPoint_rec;
+        private Point initialPosition_rec;
+        private Point offset_rec;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -160,21 +167,16 @@ namespace _8.DrawRoi
         }
 
 
-        private void roiRect_1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            startPoint_1 = e.GetPosition(roiRect_1);
-            isDragging = true;
-            selectedRect = roiRect_1;
-            roiRect_1.CaptureMouse();
-        }
+
 
         private void topRect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             startPoint_1 = e.GetPosition(topRect);
             isDragging = true;
             selectedRect = topRect;
-            Canvas.SetTop(bottomRect, Canvas.GetTop(topRect) + roiRect_1.Height);
-            topRect.CaptureMouse();
+            // Canvas.SetTop(bottomRect, Canvas.GetTop(topRect) + roiRect_1.Height-5);
+            ((Rectangle)sender).CaptureMouse();
+
         }
 
         private void bottomRect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -201,19 +203,7 @@ namespace _8.DrawRoi
             rightRect.CaptureMouse();
         }
 
-        private void roiRect_1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (isDragging && selectedRect == roiRect_1)
-            {
-                Point currentPosition = e.GetPosition(canvas_1);
 
-                double left = currentPosition.X - startPoint_1.X;
-                double top = currentPosition.Y - startPoint_1.Y;
-
-                Canvas.SetLeft(roiRect_1, left);
-                Canvas.SetTop(roiRect_1, top);
-            }
-        }
 
         private void topRect_MouseMove(object sender, MouseEventArgs e)
         {
@@ -254,16 +244,12 @@ namespace _8.DrawRoi
 
                 Canvas.SetTop(roiRect_1, top);
                 Canvas.SetTop(topRect, top);
+                Canvas.SetTop(leftRect, top);
+                Canvas.SetTop(rightRect, top);
                 //Canvas.SetTop(bottomRect, top + newHeight);
-                Debug.WriteLine(startPoint_1.Y.ToString()+" "+roiRect_1.Height.ToString() + "   "+ Canvas.GetTop(bottomRect).ToString() + "   " + currentPosition.Y.ToString()); ;
+                //Debug.WriteLine(startPoint_1.Y.ToString()+" "+roiRect_1.Height.ToString() + "   "+ Canvas.GetTop(bottomRect).ToString() + "   " + currentPosition.Y.ToString()); ;
 
 
-                //double deltaY = startPoint.Y - currentPosition.Y;
-
-                //double newTop = Canvas.GetTop(roiRect_1) - deltaY;
-                ////double newHeight = initialHeight + deltaY;
-
-                //Canvas.SetTop(roiRect, newTop);
 
             }
         }
@@ -274,17 +260,42 @@ namespace _8.DrawRoi
             {
                 Point currentPosition = e.GetPosition(canvas_1);
 
-                double bottom = currentPosition.Y - startPoint_1.Y;
-
-                if (roiRect_1.Height - bottom >= 0)
+                //double top = currentPosition.Y - startPoint_1.Y;
+                startPoint_1.Y = Canvas.GetTop(bottomRect);
+                double deltaY = currentPosition.Y - startPoint_1.Y;
+                if (currentPosition.Y >= canvas_1.ActualHeight)
                 {
-                    roiRect_1.Height -= bottom;
+                    currentPosition.Y = canvas_1.ActualHeight;
+                    deltaY = 0;
                 }
                 else
                 {
-                    roiRect_1.Height = 0;
+                    if (currentPosition.Y - Canvas.GetTop(topRect) <= 20)
+                    {
+                        currentPosition.Y = Canvas.GetTop(topRect) + 20;
+                    }
                 }
 
+
+
+                double bottom = currentPosition.Y;
+
+                double newHeight = roiRect_1.Height + deltaY;
+                if (newHeight > 20)
+                {
+                    roiRect_1.Height = newHeight;
+                }
+                else
+                {
+                    roiRect_1.Height = 20;
+                }
+
+                //Canvas.SetTop(roiRect_1, bottom);
+                Canvas.SetTop(bottomRect, bottom);
+                Canvas.SetTop(leftRect, Canvas.GetTop(topRect));
+                Canvas.SetTop(rightRect, Canvas.GetTop(topRect));
+
+                Debug.WriteLine(startPoint_1.Y.ToString() + " " + roiRect_1.Height.ToString() + "   " + Canvas.GetTop(bottomRect).ToString() + "   " + currentPosition.Y.ToString()); ;
             }
         }
 
@@ -294,10 +305,41 @@ namespace _8.DrawRoi
             {
                 Point currentPosition = e.GetPosition(canvas_1);
 
-                double left = currentPosition.X - startPoint_1.X;
+                //double top = currentPosition.Y - startPoint_1.Y;
+                startPoint_1.X = Canvas.GetLeft(leftRect);
+
+                double deltaX = startPoint_1.X - currentPosition.X;
+                if (currentPosition.X <= 0)
+                {
+                    currentPosition.X = 0;
+                    deltaX = 0;
+                }
+                else
+                {
+                    if (Canvas.GetLeft(rightRect) - currentPosition.X <= 20)
+                    {
+                        currentPosition.X = Canvas.GetLeft(rightRect) - 20;
+                    }
+                }
+
+
+
+                double left = currentPosition.X;
+
+                double newWidth = roiRect_1.Width + deltaX;
+                if (newWidth > 20)
+                {
+                    roiRect_1.Width = newWidth;
+                }
+                else
+                {
+                    roiRect_1.Width = 20;
+                }
 
                 Canvas.SetLeft(roiRect_1, left);
-                roiRect_1.Width -= left;
+                Canvas.SetLeft(leftRect, left);
+                Canvas.SetLeft(topRect, left);
+                Canvas.SetLeft(bottomRect, left);
             }
         }
 
@@ -307,9 +349,40 @@ namespace _8.DrawRoi
             {
                 Point currentPosition = e.GetPosition(canvas_1);
 
-                double right = currentPosition.X - startPoint_1.X;
+                //double top = currentPosition.Y - startPoint_1.Y;
+                startPoint_1.X = Canvas.GetLeft(rightRect);
+                double deltaX = currentPosition.X - startPoint_1.X;
+                if (currentPosition.X >= canvas_1.ActualWidth)
+                {
+                    currentPosition.X = canvas_1.ActualWidth;
+                    deltaX = 0;
+                }
+                else
+                {
+                    if (currentPosition.X - Canvas.GetLeft(leftRect) <= 20)
+                    {
+                        currentPosition.X = Canvas.GetLeft(leftRect) + 20;
+                    }
+                }
 
-                roiRect_1.Width += right;
+
+
+                double right = currentPosition.X;
+
+                double newWidth = roiRect_1.Width + deltaX;
+                if (newWidth > 20)
+                {
+                    roiRect_1.Width = newWidth;
+                }
+                else
+                {
+                    roiRect_1.Width = 20;
+                }
+
+                //Canvas.SetLeft(roiRect_1, bottom);
+                Canvas.SetLeft(rightRect, right);
+                Canvas.SetLeft(topRect, Canvas.GetLeft(leftRect));
+                Canvas.SetLeft(bottomRect, Canvas.GetLeft(leftRect));
             }
         }
 
@@ -327,18 +400,127 @@ namespace _8.DrawRoi
 
         private void image_1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            isDragging = false;
-            if (selectedRect != null)
-            {
-                selectedRect.ReleaseMouseCapture();
-                selectedRect = null;
-            }
+            //isDragging = false;
+            //if (selectedRect != null)
+            //{
+            //    selectedRect.ReleaseMouseCapture();
+            //    selectedRect = null;
+            //}
         }
 
         private void topRect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             isDragging = false;
             topRect.ReleaseMouseCapture();
+        }
+
+        private void bottomRect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+            bottomRect.ReleaseMouseCapture();
+        }
+
+        private void leftRect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+            leftRect.ReleaseMouseCapture();
+        }
+
+        private void rightRect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+            rightRect.ReleaseMouseCapture();
+        }
+
+        private void roiRect_1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint_1 = e.GetPosition(roiRect_1);
+            isDragging = true;
+            selectedRect = roiRect_1;
+            roiRect_1.CaptureMouse();
+        }
+
+
+        private void roiRect_1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging && selectedRect == roiRect_1)
+            {
+                Point currentPosition = e.GetPosition(canvas_1);
+
+                double left = currentPosition.X - startPoint_1.X;
+                double top = currentPosition.Y - startPoint_1.Y;
+
+                //计算方块左上角的位置
+
+                double marginLeft = currentPosition.X - roiRect_1.Width / 2;
+                double marginTop = currentPosition.Y - roiRect_1.Height / 2;
+
+                if (marginLeft < 0)
+                {
+                    marginLeft = 0;
+
+                }
+                else if ((marginLeft + roiRect_1.Width) > canvas_1.ActualWidth)
+                {
+                    marginLeft = canvas_1.ActualWidth - roiRect_1.Width;
+                }
+
+                if (marginTop < 0)
+                {
+                    marginTop = 0;
+
+                }
+                else if ((marginTop + roiRect_1.Height) > canvas_1.ActualHeight)
+                {
+                    marginTop = canvas_1.ActualHeight - roiRect_1.Height;
+                }
+
+
+
+                Canvas.SetLeft(roiRect_1, marginLeft);
+                Canvas.SetTop(roiRect_1, marginTop);
+
+                Canvas.SetLeft(topRect, marginLeft);
+                Canvas.SetTop(topRect, marginTop);
+
+                Canvas.SetLeft(leftRect, marginLeft);
+                Canvas.SetTop(leftRect, marginTop);
+
+                Canvas.SetLeft(bottomRect, marginLeft);
+                Canvas.SetTop(bottomRect, marginTop + roiRect_1.Height - 5);
+
+
+                Canvas.SetLeft(rightRect, marginLeft + roiRect_1.Width - 5);
+                Canvas.SetTop(rightRect, marginTop);
+
+            }
+        }
+
+        private void roiRect_1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+            roiRect_1.ReleaseMouseCapture();
+        }
+
+        private void SaveRect2_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(image_1.Width);
+            Debug.WriteLine(image_1.Height);
+            ImageSource imageSource = image_1.Source;
+            string imagePath = "D://TestFolder//Image//cat//wallpaper.jpg";
+            Uri imageUri = new Uri(System.IO.Path.GetFullPath(imagePath));
+
+            BitmapImage bitmapImage = new BitmapImage(imageUri);
+            int width = bitmapImage.PixelWidth;
+            int height = bitmapImage.PixelHeight;
+
+            // 使用获取到的宽度和高度进行后续处理
+            Debug.WriteLine("Image Width: " + width);
+            Debug.WriteLine("Image Height: " + height);
+            MessageBox.Show($"左上角X偏移：{Canvas.GetLeft(roiRect_1) * width / 300}\n" +
+                $"左上角Y偏移：{Canvas.GetTop(roiRect_1) * height / 300}\n" +
+                $"右下角X偏移：{(Canvas.GetLeft(roiRect_1) + roiRect_1.Width) * width / 300}\n" +
+                $"右下角Y偏移：{(Canvas.GetTop(roiRect_1) + roiRect_1.Height) * height / 300}", "结果");
         }
     }
 }
