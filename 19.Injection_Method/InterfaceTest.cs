c4b44030029938225755b4164e4fdbef2eebeb02
rew asd <fork213@126.com>
2024-01-29 17:29:15 +0800

    接口测试

---
+++ 19.Injection_Method/InterfaceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Injection_Method
+{
+    public interface IData
+    {
+         string GetDate();
+    }
+
+    public class Data1 : IData
+    {
+        public string GetDate()
+        {
+            return "GetData Method";
+        }
+    }
+}
---
+++ 19.Injection_Method/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace _19.Injection_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericData genericData = new GenericData();
+            genericData.WriteData();
+        }
+    }
+
+    public class GenericData
+    {
+       
+        public GenericData()
+        {
+        }
+
+        public void WriteData()
+        {
+            Data1 data1 = new Data1();
+            Console.WriteLine("Output:" + data1.GetDate()); ;
+        }
+    }
+}
