5b5b5ba5d2b0aedf96a4d582560dc3708e2e1f1c
rew asd <fork213@126.com>
2024-06-12 09:57:18 +0800

    并行集合学习1，ConcurrentQueue学习

---
+++ 25.ConcurrentTask1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _25.ConcurrentTask1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           ConcurrentQueue<int> queue=new ConcurrentQueue<int>();
+           CancellationTokenSource  cancellationTokenSource = new CancellationTokenSource();
+           
+           Task producerTask=Task.Run(async () =>
+           {
+               for (int i = 0; i < 10; i++)
+               {
+                   queue.Enqueue(i);
+                   Console.WriteLine($"Produced: {i}");
+                   await Task.Delay(1000);
+               }
+               cancellationTokenSource.Cancel();
+           }, cancellationTokenSource.Token);
+           Task consumerTask = Task.Run(async () =>
+           {
+               while (!queue.IsEmpty)
+               {
+                   if (queue.TryDequeue(out int item))
+                   {
+                       Console.WriteLine($"Consumed: {item}");
+                       await Task.Delay(3000);
+                   }
+               }
+           });
+           await Task.WhenAll(producerTask, consumerTask);
+           Console.WriteLine("Completed");
+        }
+    }
+}
