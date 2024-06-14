73f35d8256bf881caf36fc2861be7c9e331b415b
rew asd <fork213@126.com>
2024-06-14 15:42:40 +0800

    使用BlockCollection学习生产者-消费者模式

---
+++ 26.BlockCollectionTest1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _26.BlockCollectionTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BlockingCollection<int> queue = new BlockingCollection<int>();
+            
+            Task producerTask = Task.Run(async () =>
+            {
+                int i = 0;
+                while (true)
+                {
+                    queue.Add(i);
+                    Console.WriteLine($"Produced: {i}");
+                    i++;
+                    await Task.Delay(1000);
+                }
+
+            });
+            //监控producerTask是否结束，结束则重启producerTask任务
+           
+            Task consumerTask = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(3000);
+                    if (queue.TryTake(out int item))
+                    {
+                        Console.WriteLine($"Consumed: {item}");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            });
+            Task.WaitAll(producerTask, consumerTask);
+            Console.WriteLine("Completed");
+
+        }
+    }
+}
