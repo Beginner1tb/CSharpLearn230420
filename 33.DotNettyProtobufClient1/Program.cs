ef0496ea193c1938a65009eb0268c20f1f3f43f8
rew asd <fork213@126.com>
2024-09-18 18:06:37 +0800

    新增dotnetty测试的客户端和服务端

---
+++ 33.DotNettyProtobufClient1/Program.cs
@@ -0,0 +1,67 @@
+﻿using DotNetty.Codecs.Protobuf;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Channels.Sockets;
+using Google.Protobuf;
+using Prototest;  // 使用你的Protobuf命名空间
+using System;
+using System.Net;
+using DotNetty.Transport.Bootstrapping;
+
+namespace _33.DotNettyProtobufClient1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workerGroup = new MultithreadEventLoopGroup();
+            try
+            {
+                var bootstrap = new Bootstrap();
+                bootstrap.Group(workerGroup)
+                    .Channel<TcpSocketChannel>()
+                    .Handler(new ClientInitializer());
+
+                IChannel clientChannel = bootstrap.ConnectAsync(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080)).Result;
+                clientChannel.CloseCompletion.Wait();
+            }
+            finally
+            {
+                workerGroup.ShutdownGracefullyAsync().Wait();
+            }
+        }
+    }
+
+    public class ClientHandler : SimpleChannelInboundHandler<MyMessage>
+    {
+        public override void ChannelActive(IChannelHandlerContext ctx)
+        {
+            var message = new MyMessage { Id = 1, Content = "Hello, DotNetty Server!" };
+            ctx.WriteAndFlushAsync(message);
+        }
+
+        protected override async void ChannelRead0(IChannelHandlerContext ctx, MyMessage msg)
+        {
+            Console.WriteLine($"Received echo: Id = {msg.Id}, Content = {msg.Content}");
+            await ctx.CloseAsync();
+        }
+
+        public override void ExceptionCaught(IChannelHandlerContext ctx, Exception exception)
+        {
+            Console.WriteLine("Exception: " + exception.ToString());
+            ctx.CloseAsync();
+        }
+    }
+
+    public class ClientInitializer : ChannelInitializer<ISocketChannel>
+    {
+        protected override void InitChannel(ISocketChannel channel)
+        {
+            IChannelPipeline pipeline = channel.Pipeline;
+            pipeline.AddLast(new ProtobufVarint32FrameDecoder());
+            pipeline.AddLast(new ProtobufDecoder(MyMessage.Parser));
+            pipeline.AddLast(new ProtobufVarint32LengthFieldPrepender());
+            pipeline.AddLast(new ProtobufEncoder());
+            pipeline.AddLast(new ClientHandler());
+        }
+    }
+}
---
+++ 34.DotNettyProtobufServer1/Program.cs
@@ -0,0 +1,84 @@
+﻿using DotNetty.Codecs.Protobuf;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Channels.Sockets;
+using Google.Protobuf;
+using Prototest;  // 使用你的Protobuf命名空间
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetty.Buffers;
+using DotNetty.Transport.Bootstrapping;
+
+namespace _34.DotNettyProtobufServer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bossGroup = new MultithreadEventLoopGroup(1);
+            var workerGroup = new MultithreadEventLoopGroup();
+            try
+            {
+                var bootstrap = new ServerBootstrap();
+                bootstrap.Group(bossGroup, workerGroup)
+                    .Channel<TcpServerSocketChannel>()
+                    .ChildHandler(new ServerInitializer());
+
+                IChannel boundChannel = bootstrap.BindAsync(8080).Result;
+                Console.WriteLine("Server started.");
+                boundChannel.CloseCompletion.Wait();
+            }
+            finally
+            {
+                Task.WaitAll(bossGroup.ShutdownGracefullyAsync(), workerGroup.ShutdownGracefullyAsync());
+            }
+        }
+    }
+
+    public class ServerHandler : SimpleChannelInboundHandler<MyMessage>
+    {
+        protected override void ChannelRead0(IChannelHandlerContext ctx, MyMessage msg)
+        {
+            Console.WriteLine($"Received: Id = {msg.Id}, Content = {msg.Content}");
+            // Echo the message back,回写msg数据信息
+            ctx.WriteAndFlushAsync(msg);
+        }
+
+        public override void ChannelRead(IChannelHandlerContext ctx, object msg)
+        {
+
+            if (msg is IByteBuffer)
+            {
+                var messgae = (IByteBuffer)msg;
+                Console.WriteLine($"111111,Received {messgae.ToString(Encoding.UTF8)}");
+            }
+
+        }
+
+        public override void ChannelInactive(IChannelHandlerContext context)
+        {
+            // 客户端断开连接的处理
+            Console.WriteLine("Client disconnected.");
+            base.ChannelInactive(context);
+        }
+        public override void ExceptionCaught(IChannelHandlerContext ctx, Exception exception)
+        {
+            Console.WriteLine("Exception: " + exception.ToString());
+            ctx.CloseAsync();
+        }
+    }
+
+    public class ServerInitializer : ChannelInitializer<ISocketChannel>
+    {
+        protected override void InitChannel(ISocketChannel channel)
+        {
+            IChannelPipeline pipeline = channel.Pipeline;
+            //这里规定了接收数据的格式
+            pipeline.AddLast(new ProtobufVarint32FrameDecoder());
+            pipeline.AddLast(new ProtobufDecoder(MyMessage.Parser));
+            pipeline.AddLast(new ProtobufVarint32LengthFieldPrepender());
+            pipeline.AddLast(new ProtobufEncoder());
+            pipeline.AddLast(new ServerHandler());
+        }
+    }
+}
